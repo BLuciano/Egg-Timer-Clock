b382e261cbf822b4db443655998ae6bae66ce7e4
Luciano Bruzzoni <lucianobruzzoni@yahoo.com>
2018-05-04 22:59:35 -0700

    End of Egg Timer Clock App

--- Menu Bard/ViewController.swift
+++ Menu Bard/ViewController.swift
@@ -9,7 +9,55 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var timer = Timer()
+    var count = 210
+    
+    @IBOutlet weak var timeDisplay: UILabel!
+    
+    @IBAction func timeStop(_ sender: Any) {
+        if timer.isValid{
+            timer.invalidate()
+        }
+    }
+    
+    @IBAction func timePlay(_ sender: Any) {
+        if !timer.isValid{
+            timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(ViewController.processTimer), userInfo: nil, repeats: true)
+        }
+    }
+    
+    @IBAction func minusTen(_ sender: Any) {
+        count -= 10
+        if count <= 0{
+            count = 0
+            timeDisplay.text = "End of Time!"
+            timer.invalidate()
+        } else {
+            timeDisplay.text = String(count)
+        }
+    }
+    
+    @IBAction func plusTen(_ sender: Any) {
+        count += 10
+        timeDisplay.text = String(count)
+    }
+    
+    @IBAction func resetTime(_ sender: Any) {
+        count = 210
+        timeDisplay.text = String(count)
+    }
+    
+    @objc func processTimer(){
+        count -= 1
+        if count <= 0{
+            count = 0
+            timer.invalidate()
+            timeDisplay.text = "End of Time!"
+        } else {
+            timeDisplay.text = String(count)
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
